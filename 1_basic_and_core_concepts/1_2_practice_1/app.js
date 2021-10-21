d8a3411df1127bca5aedf70b58926c6fc318111c
flo.codes <falbrecht.hd@gmail.com>
2021-10-21 08:10:50 +0200

    Updated lesson one after recap

--- 1_basic_and_core_concepts/1_2_practice_1/app.js
+++ 1_basic_and_core_concepts/1_2_practice_1/app.js
@@ -5,12 +5,14 @@ const app = Vue.createApp({
       iAge: 28,
       sImageUrl:
         "https://stateless-morningchalkup-com.storage.googleapis.com/2021/03/67077468_1153365934848648_3416384834560327680_o-1.jpg",
-      sDefaultValue: "Some String",
     };
   },
   methods: {
     fnCreateRandomNumber() {
       return Math.random();
+    },
+    fnCalculateAge(iYearsToAdd) {
+      return this.iAge + iYearsToAdd;
     },
   },
 });
